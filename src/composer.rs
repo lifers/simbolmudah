2cbbdd92c3336d7f8784a2786146fa5eeca2c3fb
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2023-11-16 23:54:06 -0500

    add Incomplete sequence examples

--- src/composer.rs
+++ src/composer.rs
@@ -1,22 +1,25 @@
-use std::ffi::OsString;
-
-use windows::Win32::UI::Input::KeyboardAndMouse::VK_MENU;
-
 use crate::sequence::{key::Key, key_sequence::KeySequence};
 
+#[derive(PartialEq, Debug)]
 pub enum ComposeError {
     Incomplete,
     NotFound,
 }
 
-pub fn search(seq: KeySequence) -> Result<char, ComposeError> {
-    let ans = vec![Key::VirtualKey(VK_MENU), Key::Char('o'), Key::Char('e')];
-    let ans2 = vec![Key::VirtualKey(VK_MENU), Key::Char('>'), Key::Char('=')];
+pub fn search(seq: &KeySequence) -> Result<char, ComposeError> {
+    let ans = vec![Key::Char('o'), Key::Char('e')];
+    let pref = vec![Key::Char('o')];
+    let ans2 = vec![Key::Char('>'), Key::Char('=')];
+    let pref2 = vec![Key::Char('>')];
 
-    if seq == ans {
+    if seq == &ans {
         Ok('œ')
-    } else if seq == ans2 {
+    } else if seq == &ans2 {
         Ok('≥')
+    } else if seq == &pref {
+        Err(ComposeError::Incomplete)
+    } else if seq == &pref2 {
+        Err(ComposeError::Incomplete)
     } else {
         Err(ComposeError::NotFound)
     }
