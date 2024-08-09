82a4f81c74b951c6a1a61c0992d2ba1aff296f19
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2024-08-09 11:09:09 -0400

    try optimizing the images

--- simbolmudah-ui (Package)/Images/converter.sh
+++ simbolmudah-ui (Package)/Images/converter.sh
@@ -9,27 +9,43 @@ scales="100 125 150 200 400"
 # Input SVG file
 input_file="favicon.svg"
 
+generate_image() (
+  size=$1
+  filename=$2
+
+  temp1="temp1.png"
+  rsvg-convert -w "$size" -h "$size" -o "$temp1" "$input_file"
+  size1=$(wc -c < "$temp1")
+
+  # Try to optimize the image
+  temp2="temp2.png"
+  pngquant --strip --speed 1 256 < "$temp1" > "$temp2"
+  size2=$(wc -c < "$temp2")
+
+  if [ "$size2" -lt "$size1" ]; then
+    final="$temp2"
+  else
+    final="$temp1"
+  fi
+  mv "$final" "$filename"
+  rm -f "$temp1" "$temp2"
+  echo "Generated $filename"
+)
+
 # Clear all files that begins with "AppList"
 rm -f AppList*
 
 # Loop through each size and run rsvg-convert for each variant
 for size in $sizes; do
-  rsvg-convert -w "$size" -h "$size" -o "AppList.targetsize-${size}.png" "$input_file"
-  echo "Generated AppList.targetsize-${size}.png"
-
-  rsvg-convert -w "$size" -h "$size" -o "AppList.targetsize-${size}_altform-unplated.png" "$input_file"
-  echo "Generated AppList.targetsize-${size}_altform-unplated.png"
-
-  rsvg-convert -w "$size" -h "$size" -o "AppList.targetsize-${size}_altform-lightunplated.png" "$input_file"
-  echo "Generated AppList.targetsize-${size}_altform-lightunplated.png"
+  generate_image "$size" "AppList.targetsize-${size}.png"
+  generate_image "$size" "AppList.targetsize-${size}_altform-unplated.png"
+  generate_image "$size" "AppList.targetsize-${size}_altform-lightunplated.png"
 done
 
 # Loop through each scale and run rsvg-convert
 for scale in $scales; do
-  width=$((scale * 44 / 100))
-  height=$((scale * 44 / 100))
-  rsvg-convert -w "$width" -h "$height" -o "AppList.scale-${scale}.png" "$input_file"
-  echo "Generated AppList.scale-${scale}.png"
+  size=$((scale * 44 / 100))
+  generate_image "$size" "AppList.scale-${scale}.png"
 done
 
 # Clear all files that begins with "SmallTile"
@@ -37,10 +53,8 @@ rm -f SmallTile*
 
 # Loop through each scale and run rsvg-convert
 for scale in $scales; do
-  width=$((scale * 71 / 100))
-  height=$((scale * 71 / 100))
-  rsvg-convert -w "$width" -h "$height" -o "SmallTile.scale-${scale}.png" "$input_file"
-  echo "Generated SmallTile.scale-${scale}.png"
+  size=$((scale * 71 / 100))
+  generate_image "$size" "SmallTile.scale-${scale}.png"
 done
 
 # Clear all files that begins with "MedTile"
@@ -48,10 +62,8 @@ rm -f MedTile*
 
 # Loop through each scale and run rsvg-convert
 for scale in $scales; do
-  width=$((scale * 150 / 100))
-  height=$((scale * 150 / 100))
-  rsvg-convert -w "$width" -h "$height" -o "MedTile.scale-${scale}.png" "$input_file"
-  echo "Generated MedTile.scale-${scale}.png"
+  size=$((scale * 150 / 100))
+  generate_image "$size" "MedTile.scale-${scale}.png"
 done
 
 # Clear all files that begins with "LargeTile"
@@ -59,10 +71,8 @@ rm -f LargeTile*
 
 # Loop through each scale and run rsvg-convert
 for scale in $scales; do
-  width=$((scale * 310 / 100))
-  height=$((scale * 310 / 100))
-  rsvg-convert -w "$width" -h "$height" -o "LargeTile.scale-${scale}.png" "$input_file"
-  echo "Generated LargeTile.scale-${scale}.png"
+  size=$((scale * 310 / 100))
+  generate_image "$size" "LargeTile.scale-${scale}.png"
 done
 
 # Clear all files that begins with "StoreLogo"
@@ -70,8 +80,6 @@ rm -f StoreLogo*
 
 # Loop through each scale and run rsvg-convert
 for scale in $scales; do
-  width=$((scale * 75 / 100))
-  height=$((scale * 75 / 100))
-  rsvg-convert -w "$width" -h "$height" -o "StoreLogo.scale-${scale}.png" "$input_file"
-  echo "Generated StoreLogo.scale-${scale}.png"
+  size=$((scale * 75 / 100))
+  generate_image "$size" "StoreLogo.scale-${scale}.png"
 done
